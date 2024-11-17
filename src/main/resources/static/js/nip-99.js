482ca2ef50e63d66c023972145bb28a9f727de36
nick avlo <avlo@yahoo.com>
2024-11-17 14:33:43 +0700

    working snap

--- src/main/resources/static/js/nip-99.js
+++ src/main/resources/static/js/nip-99.js
@@ -1,30 +1,134 @@
 $(function () {
-    $("#send99").click(() => createEvent(generate99TypeScriptEvent()));
+    $("form").on('submit', (e) => e.preventDefault());
+    $("#submit99").prop("disabled", false);
+    $("#submit99").click(() => createEvent(generateCTBEventJson()));
 });
 
-function generate99TypeScriptEvent() {
+async function createEvent(typeScriptEvent) {
+    // const form = document.querySelector("#contractSubmit");
+
+    // const tempval = document.getElementById('ctbEventDto');
+    // console.log("*****************");
+    // console.log("*****************");
+    // console.log(tempval);
+    // console.log("----");
+    // console.log("*****************");
+    // console.log("*****************");
+
+    // form.setAttribute(
+    //     "ctbEventDto",
+    //     $.extend(
+    //         document.getElementById('ctbEventDto'),
+    //         // await window.nostr.signEvent(
+    //         //     generateCTBEventJson(dateNow)
+    //     )
+    // );
+
+    signEvent(typeScriptEvent).then(fullyPopulatedSignedEvent => sendData(fullyPopulatedSignedEvent));
+
+    // .then(ctbEventJson => $
+    //     .extend(
+    //         document.getElementById('ctbEventDto'),
+    //         ctbEventJson)
+    //     .then(extendedCTBEventDto => form
+    //         .setAttribute(
+    //             "ctbEventDto", extendedCTBEventDto)))
+
+    // .then(window.nostr
+    //     .signEvent(
+    //         generateCLEventJson(
+    //             dateNow,
+    //             ctbEventJson['id']))
+    //     .then(clEventJson => $
+    //         .extend(
+    //             document.getElementById('clEventDto'),
+    //             clEventJson))))
+
+    // .then(sendData())
+
+    // form.setAttribute(
+    //     "clEventDto",
+    //     // $.extend(
+    //     //     document.getElementById('clEventDto'),
+    //     await window.nostr.signEvent(
+    //         generateCLEventJson(
+    //             dateNow,
+    //             // JSON.parse(
+    //             await window.nostr.signEvent(
+    //                 generateCTBEventJson(dateNow))
+    //                 // )
+    //                 ['id'])
+    //     )
+    //     // )
+    // );
+
+}
+
+async function signEvent(event) {
+    console.log('signEvent() input: \n\n' + event + '\n\n');
+    var signedPopulatedEvent = await window.nostr.signEvent(event);
+    console.log('signEvent() output: ' + signedPopulatedEvent);
+    return signedPopulatedEvent;
+}
+
+function generateCTBEventJson() {
     const dateNow = Math.floor(Date.now() / 1000);
-    const tags = [
-        ['subject', $("#99-subject").val()],
-        ['title', $("#title").val()],
+    const ctbEventTags = [
+        ['subject', "CTBEvent subject field: " + $("#content").val()],
+        ['title', "CTBEvent title field: " + $("#content").val()],
         ['published_at', dateNow],
-        ['summary', $("#summary").val()],
-        ['location', $("#99-location").val()],
-        ['price', $("#99-number").val(), $("#99-currency").val(), $("#99-frequency").val()],
-        ['e', $("#99-e_tag").val()],
-        ['p', $("#99-p_tag").val()],
-        ['t', $("#99-t_tag").val()],
-        ['g', $("#99-g_tag").val()]
+        ['summary', "CTBEvent summary field: " + $("#content").val()],
+        ['location', "CTBEvent location field"],
+        // ['p', await window.nostr.getPublicKey(), "ws://localhost:5555", $("#role").val()]
+        // ['p', "3920fe811a15252eddc9b5a779fb35689e8ccf4c066a1988e20c54c1cae47316", "ws://localhost:5555", $("#role").val()]
+        ['p', "3920fe811a15252eddc9b5a779fb35689e8ccf4c066a1988e20c54c1cae47316"]
     ];
 
-    let event = {
+    console.log("generateCTBEventJson() checkpoint (since prohibited from console logging CTBEvent event JSON)");
+
+    return {
         id: '',
-        kind: Number($("#99-kind").val()),
+        kind: 31923,
         created_at: dateNow,
-        content: $("#content").val(),
-        tags: tags,
+        content: ("CTBEvent content field: " + $("#content").val()),
+        tags: ctbEventTags,
         pubkey: '',
         sig: ''
     }
-    return event;
+}
+
+async function generateCLEventJson(dateNow, ctbEventId) {
+    const cleEventTags = [
+        ['subject', "CLEvent subject field: " + $("#content").val()],
+        ['title', "CLEvent title field: " + $("#content").val()],
+        ['published_at', dateNow],
+        ['summary', "CLEvent summary field: " + $("#content").val()],
+        ['location', "CLEvent location field"],
+        ['price', $("payoutAmount").val(), "BTC", "1"],
+        ['p', await window.nostr.getPublicKey(), "ws://localhost:5555", $("#role").val()],
+        // ['p', "111df01ca1aa9d6f1c35953833bbe6d99a0c85b73af222e6bd305b51f2749f6f", "ws://localhost:5555", $("#role").val()]
+        ['a', "31923: " + await window.nostr.getPublicKey() + ":" + ctbEventId]
+    ];
+
+    const returnval = {
+        id: '',
+        kind: 30402,
+        created_at: dateNow,
+        content: "CLEvent content field: " + $("#content").val(),
+        tags: cleEventTags,
+        pubkey: '',
+        sig: ''
+    }
+
+    console.log("generateCLEventJson() checkpoint (since prohibited from console logging CLEventJson event JSON)");
+    return returnval;
+}
+
+async function sendData(data) {
+    const response = await fetch("contract/create", {
+        method: "POST",
+        // Set the FormData instance as the request body
+        body: data,
+    });
+    console.log(await response.json());
 }
